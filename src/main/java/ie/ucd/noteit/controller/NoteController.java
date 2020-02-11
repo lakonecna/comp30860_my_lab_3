bb481c731925aebb0693f778cbb0f4e729259527
Lucie <lucie.konecna@ucdconnect.ie>
2020-02-11 17:18:42 +0000

    playing around with http requests

--- src/main/java/ie/ucd/noteit/controller/NoteController.java
+++ src/main/java/ie/ucd/noteit/controller/NoteController.java
@@ -1,9 +1,12 @@
 package ie.ucd.noteit.controller;
 
+import ie.ucd.noteit.entities.Note;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 public class NoteController {
@@ -26,10 +29,26 @@ public class NoteController {
     }
 
     @GetMapping("/browse")
-    public String getBrowsePage() {
+    public String getBrowsePage(@RequestParam(name="name") String name,Model model) {
+        model.addAttribute("name",name);
         return "browse.html";
     }
 
+    @GetMapping("/notes")
+    public String notes(@RequestParam(name="name") String name,Model model) {
+        model.addAttribute("name",name);
+        ArrayList<Note> notes = new ArrayList<Note>();
+        notes.add(new Note(0,"Title0","I like dogs"));
+        notes.add(new Note(1,"Title1","I like cats"));
+        notes.add(new Note(2,"Title2","I like lions"));
+        model.addAttribute("notes",notes);
+        return "browse.html";
+    }
+
+    @GetMapping("/error")
+    public String getErrorPage(Model model) {
+        return "";
+    }
     /*@GetMapping("/view")
     public String getNotePage(@RequestParam(id="id") int id,Model model) {
         return model.getAttribute("id") + ".html";
